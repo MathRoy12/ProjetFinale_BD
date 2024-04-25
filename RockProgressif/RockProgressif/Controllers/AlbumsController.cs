83b4661de23816817048989c7ae15b16a21fe347
Mathieu Roy <2234476@cegepmontpetit.ca>
2024-04-25 14:26:09 -0400

    FCT Ajout Controller pour les tables importantes

---
+++ RockProgressif/RockProgressif/Controllers/AlbumsController.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RockProgressif.Data;
+using RockProgressif.Models;
+
+namespace RockProgressif.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private readonly ProgRockBDContext _context;
+
+        public AlbumsController(ProgRockBDContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Albums
+        public async Task<IActionResult> Index()
+        {
+            var progRockBDContext = _context.Albums.Include(a => a.Groupe);
+            return View(await progRockBDContext.ToListAsync());
+        }
+
+        // GET: Albums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums
+                .Include(a => a.Groupe)
+                .FirstOrDefaultAsync(m => m.AlbumId == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // GET: Albums/Create
+        public IActionResult Create()
+        {
+            ViewData["GroupeId"] = new SelectList(_context.Groupes, "GroupeId", "GroupeId");
+            return View();
+        }
+
+        // POST: Albums/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AlbumId,Nom,DatePublication,NombreVentes,NoteCritiques,GroupeId")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(album);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GroupeId"] = new SelectList(_context.Groupes, "GroupeId", "GroupeId", album.GroupeId);
+            return View(album);
+        }
+
+        // GET: Albums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            ViewData["GroupeId"] = new SelectList(_context.Groupes, "GroupeId", "GroupeId", album.GroupeId);
+            return View(album);
+        }
+
+        // POST: Albums/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AlbumId,Nom,DatePublication,NombreVentes,NoteCritiques,GroupeId")] Album album)
+        {
+            if (id != album.AlbumId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(album);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlbumExists(album.AlbumId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GroupeId"] = new SelectList(_context.Groupes, "GroupeId", "GroupeId", album.GroupeId);
+            return View(album);
+        }
+
+        // GET: Albums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums
+                .Include(a => a.Groupe)
+                .FirstOrDefaultAsync(m => m.AlbumId == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Albums == null)
+            {
+                return Problem("Entity set 'ProgRockBDContext.Albums'  is null.");
+            }
+            var album = await _context.Albums.FindAsync(id);
+            if (album != null)
+            {
+                _context.Albums.Remove(album);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlbumExists(int id)
+        {
+          return (_context.Albums?.Any(e => e.AlbumId == id)).GetValueOrDefault();
+        }
+    }
+}
